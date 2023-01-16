97cb19e08d954d5c15fe984a91d192ac78aacb80
natalieesseen <natalieesseen@gmail.com>
2023-01-16 21:38:55 +0100

    added shop item to assign role 🐸

--- currency-shop.js
+++ currency-shop.js
@@ -3,9 +3,9 @@ const force = process.argv.includes('--force') || process.argv.includes('-f');
 
 CurrencyShop.sync({ force }).then(async () => {
 	const shop = [
-		CurrencyShop.upsert({ name: 'Cafe au lait', cost: 2 }),
-		CurrencyShop.upsert({ name: 'Cake', cost: 5 }),
-        CurrencyShop.upsert({ name: 'Fries', cost: 10 }),
+		CurrencyShop.upsert({ name: 'Cafe au lait', cost: 20 }),
+		CurrencyShop.upsert({ name: 'Cake', cost: 25 }),
+		CurrencyShop.upsert({ name: 'Frog trinket', cost: 100 })
 	];
 
 	await Promise.all(shop);
